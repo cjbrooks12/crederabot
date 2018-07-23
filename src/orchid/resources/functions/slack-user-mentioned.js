fe1c23c4165eee6ec85357ddefac60323104c18d
Casey Brooks <cjbrooks12@gmail.com>
2018-07-22 21:15:09 -0500

    Adds airtable integration

--- src/orchid/resources/functions/slack-user-mentioned.js
+++ src/orchid/resources/functions/slack-user-mentioned.js
@@ -38,24 +38,19 @@ exports.handler = async (event, context) => {
                 const isPlus = match[2] === "++";
                 const reason = match[3];
 
-                return fetch(`https://slack.com/api/users.profile.get?token=${process.env.SLACK_TOKEN}&user=${userId}`, {method: "GET"})
-                    .then((response) => {
-                        return response.json();
+                return getOrCreateRecordInDatabase(userId)
+
+                    .then((record) => {
+                        return updateRecordToDatabase(userId, record.id, record.score, isPlus)
+                    })
+                    .then(() => {
+                        return getUserProfileInfo(userId)
                     })
                     .then((data) => {
-                        const userName = data.profile.real_name_normalized;
-                        return fetch("https://slack.com/api/chat.postMessage", {
-                            method: "POST",
-                            headers: {
-                                "content-type": "application/json",
-                                Authorization: `Bearer ${process.env.SLACK_TOKEN}`
-                            },
-                            body: JSON.stringify({
-                                channel: body.event.channel,
-                                text: `${userName} (${userId}) ${isPlus ? 'gains a point' : 'loses a point'} for ${reason}`
-                            })
-                        });
+                        return postMessageToSlack(data.profile.real_name_normalized, isPlus, 1, reason);
                     })
+
+                    // handle success and error
                     .then(() => ({
                         statusCode: 200,
                         body: "success"
@@ -71,3 +66,87 @@ exports.handler = async (event, context) => {
     // return error, event not supported
     return {statusCode: 404, body: "event not handled"};
 };
+
+function getUserProfileInfo(userId) {
+    return fetch(`https://slack.com/api/users.profile.get?token=${process.env.SLACK_TOKEN}&user=${userId}`, {
+        method: "GET"
+    }).then((response) => {
+        return response.json();
+    });
+}
+
+function postMessageToSlack(username, isPlus, newTotal, reason) {
+    return fetch("https://slack.com/api/chat.postMessage", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            Authorization: `Bearer ${process.env.SLACK_TOKEN}`
+        },
+        body: JSON.stringify({
+            channel: body.event.channel,
+            text: `${username} ${isPlus ? 'gains a point' : 'loses a point'} ${(reason) ? `for ${reason}` : ''}`
+        })
+    });
+}
+
+function getOrCreateRecordInDatabase(userId) {
+    return fetch(`https://api.airtable.com/v0/${process.env.AIRTABLE_TABLE_ID}?filterByFormula={userid}='${userId}'`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}`
+        }
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+            if (data.records && data.records.length === 0) {
+                return data.records[0];
+            }
+            else {
+                return fetch(`https://api.airtable.com/v0/${process.env.AIRTABLE_TABLE_ID}`, {
+                    method: "POST",
+                    headers: {
+                        Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}`
+                    },
+                    body: JSON.stringify({
+                        fields: {
+                            userid: userId,
+                            score: 0
+                        }
+                    })
+                }).then((response) => {
+                    return response.json();
+                }).then((data) => {
+                    return data.records[0];
+                })
+            }
+        }
+    );
+}
+
+function updateRecordToDatabase(userId, recordId, recordTotal, isPlus) {
+    return fetch(`https://api.airtable.com/v0/${process.env.AIRTABLE_TABLE_ID}/${recordId}`, {
+        method: "PUT",
+        headers: {
+            Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}`
+        },
+        body: JSON.stringify({
+            fields: {
+                userid: userId,
+                score: (isPlus) ? recordTotal + 1 : recordTotal - 1
+            }
+        })
+    })
+}
+
+function getNewUserTotal(userId) {
+    fetch(`https://api.airtable.com/v0/${process.env.AIRTABLE_TABLE_ID}?filterByFormula={userid}='${userId}'`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}`
+        }
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        return data.records[0].fields.score
+    });
+}
