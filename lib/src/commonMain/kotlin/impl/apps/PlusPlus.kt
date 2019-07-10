7eae6ed420e86529600fc41df6d6828adeeade17
Casey Brooks <cjbrooks12@gmail.com>
2019-07-10 15:26:57 -0500

    Implements basic database interface logic for PlusPlus app

--- lib/src/commonMain/kotlin/impl/apps/PlusPlus.kt
+++ lib/src/commonMain/kotlin/impl/apps/PlusPlus.kt
@@ -17,7 +17,8 @@ import com.caseyjbrooks.netlify.impl.environments.slack
 import com.caseyjbrooks.netlify.impl.environments.teams
 
 class PlusPlusApp(
-    override val env: EnvVar = ProductionEnv()
+    override val env: EnvVar = ProductionEnv(),
+    val db: PlusPlusDatabase = InMemoryPlusPlusDatabase()
 ) : App {
 
     override val router = HttpRootRouter {
@@ -42,45 +43,69 @@ class PlusPlusApp(
         userPatterns.forEach { mentioned ->
             message("^$mentioned$PLUS_PLUS$REASON$".toRegex()) { (user, reason) ->
                 val reasonReply = if (reason.isNotBlank()) " for ${reason.trimReason()}" else ""
-                reply("$user gained a point$reasonReply")
+
+                val newScore = db.updateScore(user, 1, reason)
+
+                reply("$user gained a point$reasonReply and is now at $newScore")
             }
             message("^$mentioned$MINUS_MINUS$REASON$".toRegex()) { (user, reason) ->
                 val reasonReply = if (reason.isNotBlank()) " for ${reason.trimReason()}" else ""
-                reply("$user lost a point$reasonReply")
+
+                val newScore = db.updateScore(user, -1, reason)
+
+                reply("$user lost a point$reasonReply and is now at $newScore")
             }
             message("^$mentioned$PLUS_EQUAL$REASON$".toRegex()) { (user, amount, reason) ->
                 val reasonReply = if (reason.isNotBlank()) " for ${reason.trimReason()}" else ""
-                reply("$user gained $amount points$reasonReply")
+
+                val newScore = db.updateScore(user, amount.toInt(), reason)
+
+                reply("$user gained $amount points$reasonReply and is now at $newScore")
             }
             message("^$mentioned$MINUS_EQUAL$REASON$".toRegex()) { (user, amount, reason) ->
                 val reasonReply = if (reason.isNotBlank()) " for ${reason.trimReason()}" else ""
-                reply("$user lost $amount points$reasonReply")
+
+                val newScore = db.updateScore(user, -1 * amount.toInt(), reason)
+
+                reply("$user lost $amount points$reasonReply and is now at $newScore")
             }
 
             message("^top (\\d+)$".toRegex()) { (count) ->
-                reply("Printing top $count")
+                val topUsers = db.getTopUsers(count.toInt())
+
+                reply("Printing top $count:${topUsers.asSeparatedList("\n> • ") { "${it.first} - ${it.second}" }}")
             }
             message("^bottom (\\d+)$".toRegex()) { (count) ->
-                reply("Printing bottom $count")
+                val bottomUsers = db.getBottomUsers(count.toInt())
+
+                reply("Printing bottom $count:${bottomUsers.asSeparatedList("\n> • ") { "${it.first} - ${it.second}" }}")
             }
 
             message("^score of$mentioned".toRegex()) { (user) ->
-                reply("Printing score of $user")
+                db.getUserScore(user)
+                    ?.let { score -> reply("$user has $score points") }
+                    ?: reply("I don't know who $user is...")
             }
             message("^(\\d*)\\s?reasons why$mentioned$".toRegex()) { (count, user) ->
-                reply("Printing $count reasons why for $user")
+                db.getUserReasons(user, count.toInt())
+                    ?.let { reasons -> reply("Printing $count reasons why for $user:${reasons.asSeparatedList("\n> • ") { "${it.first} ${it.second}" }}") }
+                    ?: reply("I don't know who $user is...")
             }
         }
     }
 
-    private fun String.trimReason() : String {
+    private fun String.trimReason(): String {
         return this
             .removePrefix("for")
             .removePrefix("because")
             .trim()
     }
 
-    suspend fun sendTestMessage(message: String) : String {
+    private fun <T> List<T>.asSeparatedList(sep: String, transform: (T) -> String): String {
+        return "$sep${this.joinToString(separator = sep, transform = transform)}"
+    }
+
+    suspend fun sendTestMessage(message: String): String {
         return call("POST", "/plus-plus/in-memory/message", message).body?.toString() ?: ""
     }
 
@@ -93,6 +118,62 @@ class PlusPlusApp(
         const val PLUS_EQUAL = "\\+=\\s*?(\\d+)"
         const val MINUS_EQUAL = "-=\\s*?(\\d+)"
         const val REASON = "(.*)"
+    }
+
+}
+
+interface PlusPlusDatabase {
+
+    suspend fun updateScore(user: String, amount: Int, reason: String): Int
+
+    suspend fun getTopUsers(count: Int): List<Pair<String, Int>>
+
+    suspend fun getBottomUsers(count: Int): List<Pair<String, Int>>
+
+    suspend fun getUserScore(user: String): Int?
+
+    suspend fun getUserReasons(user: String, count: Int): List<Pair<Int, String>>?
+
+}
+
+class InMemoryPlusPlusDatabase : PlusPlusDatabase {
+
+    private val usersMap: MutableMap<String, MutableList<Pair<Int, String>>> = mutableMapOf()
+
+    override suspend fun updateScore(user: String, amount: Int, reason: String): Int {
+        usersMap
+            .getOrPut(user) { mutableListOf() }
+            .add(amount to reason)
+
+        return usersMap[user]!!.sumBy { entry -> entry.first }
+    }
+
+    override suspend fun getTopUsers(count: Int): List<Pair<String, Int>> {
+        return usersMap
+            .entries
+            .map { it.key to it.value.sumBy { reason -> reason.first } }
+            .sortedByDescending { it.second }
+            .take(count)
+    }
+
+    override suspend fun getBottomUsers(count: Int): List<Pair<String, Int>> {
+        return usersMap
+            .entries
+            .map { it.key to it.value.sumBy { reason -> reason.first } }
+            .sortedBy { it.second }
+            .take(count)
+    }
+
+    override suspend fun getUserScore(user: String): Int? {
+        return usersMap[user]
+            ?.sumBy { it.first }
+    }
+
+    override suspend fun getUserReasons(user: String, count: Int): List<Pair<Int, String>>? {
+        return usersMap[user]
+            ?.filter { it.second.isNotBlank() }
+            ?.takeLast(count)
+            ?.reversed()
     }
 
 }
--- lib/src/commonTest/kotlin/impl/apps/PlusPlusAppTest.kt
+++ lib/src/commonTest/kotlin/impl/apps/PlusPlusAppTest.kt
@@ -19,8 +19,8 @@ class PlusPlusAppTest {
     }
 
     @Test
-    fun testNormalAppRoutes() = runTest {
-        arguments().forEach { (sentMessages, expectedReply) ->
+    fun testMessageHandling() = runTest {
+        testMessageHandlingArguments().forEach { (sentMessages, expectedReply) ->
             sentMessages.forEach { sentMessage ->
                 // pretend that each argument is a new test
                 dynamicTest(::setUp) {
@@ -30,25 +30,24 @@ class PlusPlusAppTest {
             }
         }
     }
-
-    private fun arguments(): Map<List<String>, String> {
+    private fun testMessageHandlingArguments(): Map<List<String>, String> {
         return mapOf(
             listOf(
                 "@casey++",
                 "@casey ++"
-            ) to "casey gained a point",
+            ) to "casey gained a point and is now at 1",
             listOf(
                 "@casey--",
                 "@casey --",
                 "@casey—",
                 "@casey —"
-            ) to "casey lost a point",
+            ) to "casey lost a point and is now at -1",
             listOf(
                 "@casey++ for reasons",
                 "@casey ++ for reasons",
                 "@casey++ because reasons",
                 "@casey ++ because reasons"
-            ) to "casey gained a point for reasons",
+            ) to "casey gained a point for reasons and is now at 1",
             listOf(
                 "@casey-- for reasons",
                 "@casey -- for reasons",
@@ -59,7 +58,87 @@ class PlusPlusAppTest {
                 "@casey -- because reasons",
                 "@casey— because reasons",
                 "@casey — because reasons"
-            ) to "casey lost a point for reasons"
+            ) to "casey lost a point for reasons and is now at -1"
+        )
+    }
+
+    @Test
+    fun testIncrDecr() = runTest {
+        testIncrDecrArguments().forEach { (sentMessage, expectedReply) ->
+            Clog.i("chat test: sentMessage=[$sentMessage]")
+            expectThat(app.sendTestMessage(sentMessage)).isEqualTo(expectedReply)
+        }
+    }
+    private fun testIncrDecrArguments(): List<Pair<String, String>> {
+        return listOf(
+            "@casey++" to "casey gained a point and is now at 1",
+            "@casey++" to "casey gained a point and is now at 2",
+            "@casey++" to "casey gained a point and is now at 3",
+            "@casey++" to "casey gained a point and is now at 4",
+            "@casey++" to "casey gained a point and is now at 5",
+            "@casey--" to "casey lost a point and is now at 4",
+            "@casey--" to "casey lost a point and is now at 3",
+            "@casey--" to "casey lost a point and is now at 2",
+            "@casey--" to "casey lost a point and is now at 1",
+            "@casey++" to "casey gained a point and is now at 2",
+            "@kris++" to "kris gained a point and is now at 1",
+            "@casey--" to "casey lost a point and is now at 1",
+            "@kris--" to "kris lost a point and is now at 0",
+            "@kris--" to "kris lost a point and is now at -1",
+            "@kris -= 5" to "kris lost 5 points and is now at -6",
+            "@kris += 7" to "kris gained 7 points and is now at 1"
+        )
+    }
+
+    @Test
+    fun testTopBottom() = runTest {
+        testTopBottomArguments().forEach { (sentMessage, expectedReply) ->
+            Clog.i("chat test: sentMessage=[$sentMessage]")
+            expectThat(app.sendTestMessage(sentMessage)).isEqualTo(expectedReply)
+        }
+    }
+    private fun testTopBottomArguments(): List<Pair<String, String>> {
+        return listOf(
+            "@one += 1" to "one gained 1 points and is now at 1",
+            "@two += 2" to "two gained 2 points and is now at 2",
+            "@three += 3" to "three gained 3 points and is now at 3",
+            "@four += 4" to "four gained 4 points and is now at 4",
+            "@five += 5" to "five gained 5 points and is now at 5",
+
+            "bottom 1" to "Printing bottom 1:\n> • one - 1",
+            "bottom 3" to "Printing bottom 3:\n> • one - 1\n> • two - 2\n> • three - 3",
+
+            "top 1" to "Printing top 1:\n> • five - 5",
+            "top 3" to "Printing top 3:\n> • five - 5\n> • four - 4\n> • three - 3"
+        )
+    }
+
+    @Test
+    fun testReasonsWhy() = runTest {
+        testReasonsWhyArguments().forEach { (sentMessage, expectedReply) ->
+            Clog.i("chat test: sentMessage=[$sentMessage]")
+            expectThat(app.sendTestMessage(sentMessage)).isEqualTo(expectedReply)
+        }
+    }
+    private fun testReasonsWhyArguments(): List<Pair<String, String>> {
+        return listOf(
+            "@casey ++ for one" to "casey gained a point for one and is now at 1",
+            "@casey ++ for two" to "casey gained a point for two and is now at 2",
+            "@casey ++" to "casey gained a point and is now at 3",
+            "@casey ++ for four" to "casey gained a point for four and is now at 4",
+
+            "score of @casey" to "casey has 4 points",
+            "score of @kris" to "I don't know who kris is...",
+            "@kris++" to "kris gained a point and is now at 1",
+            "score of @kris" to "kris has 1 points",
+
+            "1 reasons why @casey" to "Printing 1 reasons why for casey:\n> • 1 for four",
+            "3 reasons why @casey" to "Printing 3 reasons why for casey:\n> • 1 for four\n> • 1 for two\n> • 1 for one",
+            "4 reasons why @casey" to "Printing 4 reasons why for casey:\n> • 1 for four\n> • 1 for two\n> • 1 for one",
+
+            "1 reasons why @noel" to "I don't know who noel is...",
+            "@noel += 10 for being awesome" to "noel gained 10 points for being awesome and is now at 10",
+            "1 reasons why @noel" to "Printing 1 reasons why for noel:\n> • 10 for being awesome"
         )
     }
 
